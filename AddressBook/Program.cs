1deca023d9dcb3c9dd9f88d402d733630b4d8900
Rajeshwarikota <kotarajeswari003.com>
2023-02-07 19:09:14 +0530

    [Rajeshwari] Adding Create Contact

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -11,6 +11,8 @@ namespace AddressBook
         static void Main(string[] args)
         {
             Console.WriteLine("...Welcome to Address Book...");
+            AddressBooks Book = new AddressBooks();
+            Book.CreateContact();
             Console.ReadLine();   
         }
     }
