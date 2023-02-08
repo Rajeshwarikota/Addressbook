e6c1f19f45abe1d1d9b0343cf730dc1b73ac8d49
Rajeshwarikota <kotarajeswari003.com>
2023-02-08 13:08:31 +0530

    [Rajeshwari] Adding Multiple Contacts

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -15,7 +15,7 @@ namespace AddressBook
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("1.Create Contact \n2.Add Contact \n3.Display Contact \n4.Edit Contact \n5.Delete Contact \n22.Exit");
+                Console.WriteLine("1.Create Contact \n2.Add Contact \n3.Display Contact \n4.Edit Contact \n5.Delete Contact \n6.MulpliContact\n22.Exit");
                 int choice = Convert.ToInt32(Console.ReadLine());
                 switch (choice)
                 {
@@ -33,6 +33,9 @@ namespace AddressBook
                         break;
                     case 5:
                         Book.DeleteContact();
+                        break;
+                    case 6:
+                        Book.AddMultipleContact();
                         break;
                     case 22:
                         flag = false;
